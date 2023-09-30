be49f46b1b2d84b5617a9937394ab2e146f5cedc
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2023-09-30 15:16:53 -0500

    add checking status

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import { AuthRouter } from "../auth/router/AuthRouter"
 import { JournalRoutes } from "../journal/router/JournalRoutes"
+import { useSelector } from "react-redux"
+import { CheckingAuth } from "../ui/components/CheckingAuth"
 
 export const AppRoutes = () => {
+
+    const {status} = useSelector( state => state.auth)
+
+    if(status === 'checking'){
+        return  <CheckingAuth/>
+    }
+
     return (
         <Routes>
             {/* Rutas de autenticación */}
