69d51b23bdc2fb74619bf21603a11a5e004fb8c9
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2023-10-07 15:58:31 -0500

    add visor images

--- src/journal/views/ImageGallery.jsx
+++ src/journal/views/ImageGallery.jsx
@@ -1,20 +1,59 @@
 import { ImageList, ImageListItem } from "@mui/material";
+import  { useState } from "react";
 
+export const ImageGallery = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
 
-export const ImageGallery = ({images}) => {
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseImage = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
-      {images?.map((image) => (
-        <ImageListItem key={image}>
-          <img
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt='Imagen de nota'
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
-}
+    <div>
+      <ImageList sx={{ width: "100%", height: 500 }} cols={4} rowHeight={200}>
+        {images?.map((image) => (
+          <ImageListItem
+            key={image}
+            onClick={() => handleImageClick(image)}
+            style={{ cursor: "pointer" }}
+          >
+            <img
+              src={`${image}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt="Imagen de nota"
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
 
+      {selectedImage && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.8)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 9999,
+          }}
+          onClick={handleCloseImage}
+        >
+          <img
+            src={selectedImage}
+            alt="Imagen ampliada"
+            style={{ maxWidth: "90%", maxHeight: "90%" }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
